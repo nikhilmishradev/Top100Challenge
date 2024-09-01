c876f32a93386f8c49e393b496ca53f4d44e2826
nikhil <chandragupt557@gmail.com>
2024-09-01 18:58:24 +0530

    - created NumberIntoWords Challenge Task - the Max range of the Number Conversion into Words is from 0 to 99999

---
+++ app/src/main/java/com/androidtechguru/top100challenge/ui/activity/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.androidtechguru.top100challenge.ui.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.androidtechguru.top100challenge.ui.theme.Top100ChallengeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Top100ChallengeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Top100ChallengeTheme {
+        Greeting("Android")
+    }
+}
