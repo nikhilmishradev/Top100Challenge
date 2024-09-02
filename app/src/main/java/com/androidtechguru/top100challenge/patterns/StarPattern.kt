21704311d1442f047119af750b2d14ac1d178c39
Nikhil Mishra <39642193+nikhilmishradev@users.noreply.github.com>
2024-09-02 13:05:32 +0530

    Update StarPattern.kt

--- app/src/main/java/com/androidtechguru/top100challenge/patterns/StarPattern.kt
+++ app/src/main/java/com/androidtechguru/top100challenge/patterns/StarPattern.kt
@@ -1,1 +1,56 @@
-package com.androidtechguru.top100challenge.patternsfun main() {    val pattern = Patterns()    pattern.rightAngleTriangle()    pattern.invertedRightAngleTriangle()    pattern.rightAngleTriangleMirrored(10)    pattern.triangle()}class Patterns {        fun rightAngleTriangle(maxRow: Int = 5) {        repeat(maxRow) { row ->            repeat(row + 1) {                print("*")            }            println()        }    }        fun invertedRightAngleTriangle(maxRow: Int = 5) {        repeat(maxRow) { row ->            repeat(maxRow - row) {                print("*")            }            println()        }    }        fun rightAngleTriangleMirrored(maxRow: Int = 5) {        repeat(maxRow) { row ->            repeat(maxRow) { col ->                if (col < maxRow - row - 1) {                    print(" ")                } else {                    print("*")                }            }            println()        }    }        fun triangle(maxRow: Int = 5) {        repeat(maxRow) { row ->            repeat(maxRow - row) {                print(" ")            }            repeat(row + 1) {                print("* ")            }            println()        }    }    }
+package com.androidtechguru.top100challenge.patterns
+
+fun main() {
+    val pattern = Patterns()
+    pattern.rightAngleTriangle()
+    pattern.invertedRightAngleTriangle()
+    pattern.rightAngleTriangleMirrored(10)
+    pattern.triangle()
+}
+
+class Patterns {
+    
+    fun rightAngleTriangle(maxRow: Int = 5) {
+        repeat(maxRow) { row ->
+            repeat(row + 1) {
+                print("*")
+            }
+            println()
+        }
+    }
+    
+    fun invertedRightAngleTriangle(maxRow: Int = 5) {
+        repeat(maxRow) { row ->
+            repeat(maxRow - row) {
+                print("*")
+            }
+            println()
+        }
+    }
+    
+    fun rightAngleTriangleMirrored(maxRow: Int = 5) {
+        repeat(maxRow) { row ->
+            repeat(maxRow) { col ->
+                if (col < maxRow - row - 1) {
+                    print(" ")
+                } else {
+                    print("*")
+                }
+            }
+            println()
+        }
+    }
+    
+    fun triangle(maxRow: Int = 5) {
+        repeat(maxRow) { row ->
+            repeat(maxRow - row) {
+                print(" ")
+            }
+            repeat(row + 1) {
+                print("* ")
+            }
+            println()
+        }
+    }
+    
+}
